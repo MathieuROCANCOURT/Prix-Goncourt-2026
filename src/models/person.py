5f5f8f59f9b4bd5c4ebf3ca7707683760abf1fda
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-15 12:03:30 +0200

    feat: Add Person class with fields and __str__ method.

---
+++ src/models/person.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe abstraite Person, mère de Jury, JuryChair et Author
+"""
+
+from abc import ABC
+from dataclasses import dataclass
+
+
+@dataclass
+class Person(ABC):
+    """Personne liée au prix Goncourt : enseignant ou élève."""
+    first_name: str
+    last_name: str
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
