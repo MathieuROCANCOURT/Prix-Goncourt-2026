9bcb85c0ea6b1a3184d4644d4482ee436524714b
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-15 13:40:39 +0200

    feat: Add ListJury class.

---
+++ src/models/list_jury.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Book
+"""
+
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
+from dataclasses import dataclass, field
+from datetime import date
+
+# pour éviter une circularité des imports à l'exécution,
+# les classes Jury, JuryChair et Book important la classe ListJury
+if TYPE_CHECKING:
+    from .jury import Jury
+    from .jury_chair import JuryChair
+    from .book import Book
+
+
+@dataclass
+class ListJury:
+    jury_chair: JuryChair
+    list_jury: list[Jury]
+    list_book_selected: list[Book] = field(default_factory=list, init=True)
