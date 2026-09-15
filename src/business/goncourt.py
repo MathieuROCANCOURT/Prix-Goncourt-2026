af8f7fa6c2cd87aeb08234e8908326a3d1f98fd1
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-15 13:49:27 +0200

    feat: Add Goncourt class.

---
+++ src/business/goncourt.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe School
+"""
+
+from dataclasses import dataclass, field
+
+from models.book import Book
+
+
+@dataclass
+class Goncourt:
+    books: list[Book] = field(default_factory=list, init=True)
+
+    def display_book_list(self) -> None:
+        """Affichage de la liste des livres avec pour chacun d'eux :
+        - leur auteur
+        - l'éditeur"""
+        for book in self.books:
+            print(book)
+
+    def init_static(self) -> None:
+        """Initialisation d'un jeu de test pour le prix Goncourt."""
