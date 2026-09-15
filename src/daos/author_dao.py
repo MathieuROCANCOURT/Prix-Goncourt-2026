ed6f3b9d40dbbfd3dc69d533822b7386343182eb
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-15 14:43:52 +0200

    feat: Add AuthorDao class.

---
+++ src/daos/author_dao.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Dao[Author]
+"""
+from models.author import Author
+from daos.dao import Dao
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class AuthorDao(Dao[Author]):
+    def create(self, author: Author) -> int:
+        """Crée en BD l'entité Author correspondant au livre author
+
+        :param author: à créer sous forme d'entité Author en BD
+        :return: l'id de l'entité insérée en BD (0 si la création a échoué)
+        """
+        with Dao.connection.cursor() as cursor:
+            return cursor.lastrowid > 0
+
+    def read(self, id_author: int) -> Optional[Author]:
+        """Renvoit l'auteur correspondant à l'entité dont l'id est au_id_author
+           (ou None s'il n'a pu être trouvé)"""
+        author: Optional[Author]
+
+        with Dao.connection.cursor():
+            return None
+
+    def update(self, author: Author) -> bool:
+        """Met à jour en BD l'entité Author correspondant à author, pour y correspondre
+
+        :param author: auteur déjà mis à jour en mémoire
+        :return: True si la mise à jour a pu être réalisée
+        """
+        with Dao.connection.cursor() as cursor:
+            return cursor.rowcount > 0
+
+    def delete(self, author: Author) -> bool:
+        """Supprime en BD l'entité author correspondant à author
+
+        :param author: livre dont l'entité Author correspondante est à supprimer
+        :return: True si la suppression a pu être réalisée
+        """
+        pass  # Don't necessary to delete an author.
