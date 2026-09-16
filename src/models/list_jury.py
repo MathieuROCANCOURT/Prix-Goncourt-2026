baac755f4d18ef3d5bdf23f19480c4fb581bea06
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-16 11:26:17 +0200

    feat: Add index_jury_chair method in ListJury class.

--- src/models/list_jury.py
+++ src/models/list_jury.py
@@ -9,16 +9,23 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 from dataclasses import dataclass, field
 
+from .jury_chair import JuryChair
+
 # pour éviter une circularité des imports à l'exécution,
-# les classes Jury, JuryChair et Book important la classe ListJury
+# les classes Jury et Book important la classe ListJury
 if TYPE_CHECKING:
     from .jury import Jury
-    from .jury_chair import JuryChair
     from .book import Book
 
 
 @dataclass
 class ListJury:
-    jury_chair: JuryChair
     list_jury: list[Jury]
     list_book_selected: list[Book] = field(default_factory=list, init=True)
+
+    def index_jury_chair(self) -> int:
+        for jury in self.list_jury:
+            if isinstance(jury, JuryChair):
+                return self.list_jury.index(jury)
+
+        return -1
