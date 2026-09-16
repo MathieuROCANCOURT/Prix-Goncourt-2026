2e7c0c2117733a0095f8f414421efaffa5e21532
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-16 14:46:27 +0200

    refactor: Remove get_all_book_id_selected from Jury class.

--- src/models/jury.py
+++ src/models/jury.py
@@ -10,6 +10,7 @@ from typing import Optional
 from daos import book_dao
 from daos import jury_dao
 from .book import Book
+from .list_jury import ListJury
 from .person import Person
 
 
@@ -18,21 +19,13 @@ class Jury(Person):
     id: Optional[int] = field(default=None, init=False)
     voted_id_book: Optional[int] = field(default=None, init=False)
 
-    @staticmethod
-    def get_all_book_id_selected(list_book):
-        list_id_book = []
-        for book in list_book:
-            list_id_book.append(str(book.get_id))
-
-        return list_id_book
-
     def vote(self, list_book):
         vote_confirm: bool = False
 
         while not vote_confirm:
             id_book_vote: str = input("Pour quelle livre voulez-vous voter (en saisissant l'id) ?")
 
-            while id_book_vote not in self.get_all_book_id_selected(list_book):
+            while id_book_vote not in ListJury.get_all_book_id_selected(list_book):
                 id_book_vote = input("Vous n'avez pas saisie de choix parmi la liste des livre.\nVeuillez réessayer.")
 
             book_vote: Book | None = book_dao.BookDao().read(int(id_book_vote))
