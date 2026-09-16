849e5f0f3be4c0c3362e0006c556ab5108897d1e
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-16 09:11:21 +0200

    fix: Modify type 'bo_editor_price' in book table.

--- bdd/goncourt_price.sql
+++ bdd/goncourt_price.sql
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `book` (
   `bo_main_people` TEXT,
   `bo_publication_date` DATE NOT NULL,
   `bo_nb_pages` SMALLINT NOT NULL,
-  `bo_editor_price` DECIMAL(3,2) NOT NULL,
+  `bo_editor_price` DECIMAL(5,2) NOT NULL,
   `bo_selected_to_turn` SMALLINT NOT NULL DEFAULT 1,
   `bo_id_editor` INT NOT NULL,
   `bo_id_author` INT NOT NULL,
