5a49c88bca4b8f712d9a5f17a7aa119aa596e4ca
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-16 09:15:58 +0200

    feat: Add read_all_book_from-dao method in Goncourt class.

--- src/business/goncourt.py
+++ src/business/goncourt.py
@@ -13,6 +13,9 @@ from models.book import Book
 class Goncourt:
     books: list[Book] = field(default_factory=list, init=True)
 
+    def read_all_book_from_dao(self):
+        self.books = book_dao.BookDao().read_all()
+
     def display_book_list(self) -> None:
         """Affichage de la liste des livres avec pour chacun d'eux :
         - leur auteur
