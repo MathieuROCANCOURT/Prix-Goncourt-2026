1a7a62c0ed3514f354f69ce4e493c0fba589f4b5
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-16 15:59:41 +0200

    feat: Complete update method in BookDao class.

--- src/daos/book_dao.py
+++ src/daos/book_dao.py
@@ -3,6 +3,7 @@
 """
 Classe Dao[Book]
 """
+from daos import editor_dao, author_dao
 from daos.author_dao import AuthorDao
 from daos.editor_dao import EditorDao
 from models.book import Book
@@ -57,7 +58,7 @@ class BookDao(Dao[Book]):
                 SELECT * FROM book
                 WHERE bo_id_book = %s;
                 """
-            cursor.execute(sql, (id_book, ))
+            cursor.execute(sql, (id_book,))
             record: dict[str, Any] | tuple[Any] | None = cursor.fetchone()
 
             if isinstance(record, dict):
@@ -92,8 +93,45 @@ class BookDao(Dao[Book]):
         :param book: livre déjà mis à jour en mémoire
         :return: True si la mise à jour a pu être réalisée
         """
-        with Dao.connection.cursor() as cursor:
-            return cursor.rowcount > 0
+        try:
+            with Dao.connection.cursor() as cursor:
+                sql = """
+                        UPDATE book
+                        SET
+                            bo_title=%s,
+                            bo_isbn=%s,
+                            bo_resume=%s,
+                            bo_main_people==%s,
+                            bo_publication_date=%s,
+                            bo_nb_pages=%s,
+                            bo_editor_price=%s,
+                            bo_selected_to_turn=%s,
+                            bo_id_editor=%s,
+                            bo_id_author=%s
+                        WHERE bo_id_book = %s;
+                    """
+                cursor.execute(sql, (book.title,
+                                     book.isbn,
+                                     book.resume,
+                                     book.list_main_people,
+                                     book.publish_date,
+                                     book.nb_pages,
+                                     book.price,
+                                     book.editor.id,
+                                     book.author.id,
+                                     book.get_id))
+
+                if book.editor.id is not None and book.editor != editor_dao.EditorDao().read(book.editor.id):
+                    editor_dao.EditorDao().update(book.editor)
+                if book.author.id is not None and book.author != author_dao.AuthorDao().read(book.author.id):
+                    author_dao.AuthorDao().update(book.author)
+
+                return cursor.rowcount > 0
+
+        except Exception as e:
+            print(f"Exception : {e}")
+
+        return True
 
     def delete(self, book: Book) -> bool:
         pass  # No necessary to delete a book.
