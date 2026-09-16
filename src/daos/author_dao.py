9c529d5e255ecb04a78ffd3db09782366d578bd9
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-16 16:21:31 +0200

    feat: Complete update method in AuthorDao class.

--- src/daos/author_dao.py
+++ src/daos/author_dao.py
@@ -57,8 +57,28 @@ class AuthorDao(Dao[Author]):
         :param author: auteur déjà mis à jour en mémoire
         :return: True si la mise à jour a pu être réalisée
         """
-        with Dao.connection.cursor() as cursor:
-            return cursor.rowcount > 0
+        try:
+            with Dao.connection.cursor() as cursor:
+                sql = """
+                        UPDATE author
+                        JOIN person on person.pe_id_person = author.au_id_person
+                        SET
+                            person.pe_first_name=%s,
+                            person.pe_last_name=%s,
+                            au_biography=%s
+                        WHERE author.au_id_author = %s;
+                    """
+                cursor.execute(sql, (author.first_name,
+                                     author.last_name,
+                                     author.biography,
+                                     author.id))
+
+                return cursor.rowcount > 0
+
+        except Exception as e:
+            print(f"Exception : {e}")
+
+        return True
 
     def delete(self, author: Author) -> bool:
         """Supprime en BD l'entité author correspondant à author
